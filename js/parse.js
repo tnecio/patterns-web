1a1696989afceb1581d95773917fc0ca79fef5d6
Tomasz Necio <tomasz.necio@gmail.com>
2021-01-17 01:45:48 +0100

    Parsing POSCARs

---
+++ js/parse.js
@@ -0,0 +1,110 @@
+const PERIODIC_TABLE = [ "Unknown",
+    "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al",
+    "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe",
+    "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y",
+    "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb",
+    "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd",
+    "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir",
+    "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac",
+    "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No",
+    "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl",
+    "Mc", "Lv", "Ts", "Og"
+];
+
+function parsePoscar(s, species) {
+    let res = {
+        name: "Layer from POSCAR",
+        height: 1,
+        atoms: [],
+        cell: [
+            {x: 1, y: 0},
+            {x: 0, y: 1}
+        ],
+        rotation: 0
+    };
+
+    let lines = s.split("\n");
+
+    // 1. System name
+    res.name = lines[0];
+
+    // 2. Scale factor
+    let scale = Number(lines[1]);
+
+    // 3-5. Lattice vectors in Angstroms
+    let vecs = [];
+    for (let i = 0; i < 3; i++) {
+        let v = lines[2 + i].trim().split(/\s+/).map(x => Number(x));
+        if (v.length !== 3) {
+            return {error: "Bad length of vector in line " + (i + 1) + ": " + v.length};
+        }
+        vecs.push(v);
+    }
+    res.cell[0].x = vecs[0][0];
+    res.cell[0].y = vecs[0][1];
+    res.cell[1].x = vecs[1][0];
+    res.cell[1].y = vecs[1][1];
+    res.height = vecs[2][2];
+
+    // 6.
+    let offset = 5;
+    let line = lines[offset].trim().split(/\s+/);
+    if (isNaN(Number(line[0]))) {
+        // Optional: atomic species names
+        species = line.map(x => PERIODIC_TABLE.indexOf(x)); // Convert to atomic numbers
+        for (let i = 0; i < species.length; i++) {
+            if (species[i] === -1) {
+                return {error: "Bad species symbols in the file"};
+            }
+        }
+        offset += 1;
+    } else {
+        // we need species from the second argument
+        if (!species || !species[0]) {
+            speciesPrompted = prompt("Atomic element symbols were not supplied in the POSCAR file. Please enter them here as a SPACE-separated list");
+            if (speciesPrompted === null) {
+                return {error: "Species not supplied"};
+            }
+            species = speciesPrompted.trim().split(/\s+/).map(x => PERIODIC_TABLE.indexOf(x));
+        } else {
+            for (let i = 0; i < species.length; i++) {
+                if (PERIODIC_TABLE.indexOf(species[i]) === -1) {
+                    console.log(species);
+                    return {error: "No such element symbol " + species[i]};
+                }
+            }
+        }
+    }
+
+    // 7. Atomic species counts
+    let counts = lines[offset].trim().split(/\s+/).map(x => Number(x));
+    if (counts.length !== species.length) {
+        return {error: "Species names and counts arrays lengths do not match"};
+    }
+    offset++;
+
+    // 8. Optional: Selective dynamics
+    if (lines[offset][0].toLowerCase() === "s") {
+        offset++;
+    }
+
+    // 9. Cartesian or Direct
+    if (lines[offset][0].toLowerCase() === "c" || lines[offset][0].toLowerCase() === "k") {
+        return {error: "Cartesian coordinates not yet supported"};
+    }
+    offset++;
+
+    // 10+. Atoms
+    for (let i = 0; i < species.length; i++) {
+        let el = species[i];
+        let count = counts[i];
+        for (let j = 0; j < count; j++) {
+            let vec = lines[offset].trim().split(/\s+/).slice(0, 3).map(x => Number(x));
+            let atom = { el: el, x: vec[0], y: vec[1], z: vec[2] };
+            res.atoms.push(atom);
+            offset++;
+        }
+    }
+
+    return res;
+}
--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
+function get_random_color() {
+    var h = 1 + Math.random() * (360 - 1);
+    var s = 100;
+    var l = 30;
+    return 'hsl(' + h + ',' + s + '%,' + l + '%)';
+}
+
+var colors = [];
+for (var i = 1; i < 300; i++) {
+    colors.push(get_random_color());
+}
+
 Vue.component('atom', {
     props: ['data', 'index', 'transform', 'retransform'],
     // retransform is used to transform back to normal coordinates so that circles are circles
     // transform is used to move positions again to where they should be in the transformed pattern
     template: `
     <g>
-        <circle :cx='getX(transform, data, 0, 0)' :cy='getY(transform, data, 0, 0)' r='0.05' fill="blue" :transform="'matrix (' + retransform + ')'"></circle>
+        <circle :cx='getX(transform, data, 0, 0)' :cy='getY(transform, data, 0, 0)' r='0.05' :fill="getColor(data)" :transform="'matrix (' + retransform + ')'"></circle>
         <!-- deal with overflow:visible being broken -->
-        <circle :cx='getX(transform, data, 1, 0)' :cy='getY(transform, data, 1, 0)' r='0.05' fill="blue" :transform="'matrix (' + retransform + ')'"></circle>
-        <circle :cx='getX(transform, data, -1, 0)' :cy='getY(transform, data, -1, 0)' r='0.05' fill="blue" :transform="'matrix (' + retransform + ')'"></circle>
-        <circle :cx='getX(transform, data, 0, 1)' :cy='getY(transform, data, 0, 1)' r='0.05' fill="blue" :transform="'matrix (' + retransform + ')'"></circle>
-        <circle :cx='getX(transform, data, 0, -1)' :cy='getY(transform, data, 0, -1)' r='0.05' fill="blue" :transform="'matrix (' + retransform + ')'"></circle>
-        <circle :cx='getX(transform, data, 1, 1)' :cy='getY(transform, data, 1, 1)' r='0.05' fill="blue" :transform="'matrix (' + retransform + ')'"></circle>
-        <circle :cx='getX(transform, data, 1, -1)' :cy='getY(transform, data, 1, -1)' r='0.05' fill="blue" :transform="'matrix (' + retransform + ')'"></circle>
-        <circle :cx='getX(transform, data, -1, 1)' :cy='getY(transform, data, -1, 1)' r='0.05' fill="blue" :transform="'matrix (' + retransform + ')'"></circle>
-        <circle :cx='getX(transform, data, -1, -1)' :cy='getY(transform, data, -1, -1)' r='0.05' fill="blue" :transform="'matrix (' + retransform + ')'"></circle>
+        <circle :cx='getX(transform, data, 1, 0)' :cy='getY(transform, data, 1, 0)' r='0.05' :fill="getColor(data)" :transform="'matrix (' + retransform + ')'"></circle>
+        <circle :cx='getX(transform, data, -1, 0)' :cy='getY(transform, data, -1, 0)' r='0.05' :fill="getColor(data)" :transform="'matrix (' + retransform + ')'"></circle>
+        <circle :cx='getX(transform, data, 0, 1)' :cy='getY(transform, data, 0, 1)' r='0.05' :fill="getColor(data)" :transform="'matrix (' + retransform + ')'"></circle>
+        <circle :cx='getX(transform, data, 0, -1)' :cy='getY(transform, data, 0, -1)' r='0.05' :fill="getColor(data)" :transform="'matrix (' + retransform + ')'"></circle>
+        <circle :cx='getX(transform, data, 1, 1)' :cy='getY(transform, data, 1, 1)' r='0.05' :fill="getColor(data)" :transform="'matrix (' + retransform + ')'"></circle>
+        <circle :cx='getX(transform, data, 1, -1)' :cy='getY(transform, data, 1, -1)' r='0.05' :fill="getColor(data)" :transform="'matrix (' + retransform + ')'"></circle>
+        <circle :cx='getX(transform, data, -1, 1)' :cy='getY(transform, data, -1, 1)' r='0.05' :fill="getColor(data)" :transform="'matrix (' + retransform + ')'"></circle>
+        <circle :cx='getX(transform, data, -1, -1)' :cy='getY(transform, data, -1, -1)' r='0.05' :fill="getColor(data)" :transform="'matrix (' + retransform + ')'"></circle>
     </g>
 `,
     methods: {
@@ -29,6 +41,10 @@ Vue.component('atom', {
             const x = Number(data.x) + Number(xoffset);
             const y = Number(data.y) + Number(yoffset);
             return v[1] * x + v[3] * y;
+        },
+
+        getColor: function (data) {
+            return colors[data.el];
         }
     }
 })
@@ -36,7 +52,7 @@ Vue.component('atom', {
 Vue.component('layerPattern', {
     props: ['data', 'index', 'settings'],
     template: `
-<pattern :id="'layer' + index" :width="settings.scale * 0.1" :height="settings.scale * 0.1" viewBox="0 0 1 1" overflow="visible" :patternTransform="'rotate(' + data.rotation + ' 50 50) matrix(' + getTransformMatrix() + ')'">
+<pattern :id="'layer' + index" :width="1 / settings.scale" :height="1 / settings.scale" viewBox="0 0 1 1" overflow="visible" :patternTransform="'rotate(' + data.rotation + ' 50 50) matrix(' + getTransformMatrix() + ')'">
     <atom
         v-if="settings.showAtoms" 
         v-for="(atom, atomIndex) in data.atoms"
@@ -83,47 +99,86 @@ Vue.component('layerRect', {
 
 Vue.component('atomControl', {
     props: ['data', 'index'],
+    created () {
+        this.PERIODIC_TABLE = PERIODIC_TABLE;
+    },
     template: `
-<div class="atomControl">
-    <label for="x">x  <input v-model="data.x" id="x" name="x" type="number" min="0" max="1" step="0.01"></label>
-    <label for="y">y  <input v-model="data.y" id="y" name="y" type="number" min="0" max="1" step="0.01"></label>
-    <button @click="$emit('remove-atom', index)">X</button>
-</div>
+<tr class="atomControl">
+<td>
+    {{ PERIODIC_TABLE[data.el] }}
+</td><td>
+    <label for="x">$$x$$  <input v-model="data.x" name="x" type="number" min="0" max="1" step="0.01"></label>
+    <label for="y">$$y$$  <input v-model="data.y" name="y" type="number" min="0" max="1" step="0.01"></label>
+    <button @click="$emit('remove-atom', index)">❌</button>
+</td>
+</tr>
 `
 })
 
 Vue.component('layerControl', {
-    props: ['data', 'index'],
+    props: ['l', 'index'],
+
+    created () {
+        this.PERIODIC_TABLE = PERIODIC_TABLE;
+    },
+
+    data: function() { return {
+        el: "H"
+    }; },
+
     template: `
 <div class="layerControl">
     <form novalidate class="layerPropertiesControl">
-    <label for="v00">$$v_1^x$$ (Å) <input v-model="data.cell[0].x" id="v00" name="v00" type="number" step="0.1"></label>
-    <label for="v01">$$v_1^y$$ (Å) <input v-model="data.cell[0].y" id="v01" name="v01" type="number" step="0.1"></label>
-    <label for="v10">$$v_2^x$$ (Å) <input v-model="data.cell[1].x" id="v10" name="v10" type="number" step="0.1"></label>
-    <label for="v11">$$v_2^y$$ (Å) <input v-model="data.cell[1].y" id="v11" name="v11" type="number" step="0.1"></label>
+    <label for="v00">$$v_1^x$$ (Å) <input v-model="l.cell[0].x" name="v00" type="number" step="0.1"></label>
+    <label for="v01">$$v_1^y$$ (Å) <input v-model="l.cell[0].y" name="v01" type="number" step="0.1"></label>
+    <label for="v10">$$v_2^x$$ (Å) <input v-model="l.cell[1].x" name="v10" type="number" step="0.1"></label>
+    <label for="v11">$$v_2^y$$ (Å) <input v-model="l.cell[1].y" name="v11" type="number" step="0.1"></label>
     
-    <label for="rot">Rotation (°) <input v-model="data.rotation" id="rot" name="rot" type="number" step="1"></label>
+    <label for="rot">Rotation (°) <input v-model="l.rotation" name="rot" type="number" step="1"></label>
     </form>
     
-    <div class="atomsControl">
-    <atomControl
-        v-for="(atom, index) in data.atoms"
-        v-bind:key="index" v-bind:index="index" v-bind:data="atom"
-        @remove-atom="removeAtom"
-    ></atomControl>
-    <button @click="addAtom('C')">Add atom</button>
-    <button @click="$emit('remove-layer', index)">Remove layer</button>
-    </div>
+    <form novalidate>
+        <table class="atomsControl">
+            <atomControl
+                v-for="(atom, index) in l.atoms"
+                v-bind:key="index" v-bind:index="index" v-bind:data="atom"
+                @remove-atom="removeAtom"
+            ></atomControl>
+        </table>
+    </form>
+    
+    <form v-on:submit.prevent="addAtom()"> <!-- TODO: WHY TWICE?? -->
+        <label for="el">Element (Z/symbol) <input v-model="el" id="el" name="el" type="text"></label>
+        <button type="submit" title="Add atom">Add atom</button>
+        <button @click="$emit('remove-layer', index)" type="button">Remove layer</button>
+        <span style="color:red;" v-if="!inputIsElement()">No such element</span>
+    </form>
+    
 </div>    
 `,
 
     methods: {
-        addAtom: function (specie) {
-            this.data.atoms.push({el: specie, x: 0, y: 0, z: 0});
+        inputIsElement: function() {
+            const el = this.el;
+            return (el > 0 && el < 119 && Number.isInteger(Number(el))) || (PERIODIC_TABLE.indexOf(el) !== -1);
+        },
+
+        addAtom: function () {
+            if (!this.inputIsElement()) {
+                return;
+            }
+            var Z = Number(this.el);
+            if (isNaN(Z)) {
+                Z = PERIODIC_TABLE.indexOf(this.el);
+                if (Z === -1) {
+                    return;
+                }
+            }
+            this.l.atoms.push({el: Z, x: 0, y: 0, z: 0});
         },
 
         removeAtom: function (index) {
-            this.data.atoms.splice(index, 1);
+            this.l.atoms.splice(index, 1);
         }
     }
 })
@@ -131,13 +186,17 @@ Vue.component('layerControl', {
 let lp = new Vue({
     el: "#lp",
 
+    created () {
+        this.parsePoscar = parsePoscar;
+    },
+
     data: {
         layers: [
             {
                 name: "Sample Layer",
                 height: 1,
                 atoms: [
-                    {el: "C", x: 0, y: 0, z: 0}
+                    {el: 6, x: 0, y: 0, z: 0}
                 ],
                 cell: [
                     {x: 1, y: 0},
@@ -146,10 +205,20 @@ let lp = new Vue({
                 rotation: 0
             }
         ],
+
         settings: {
             showAtoms: true,
             showCells: true,
-            scale: 1
+            scale: 10
+        },
+
+        inputs: {
+            poscar: "",
+            poscarSpecies: ""
+        },
+
+        messages: {
+            poscarError: ""
         }
     },
 
@@ -173,15 +242,32 @@ let lp = new Vue({
     </figure>
 
     <nav>
-    <label for="showAtoms">Show Atoms<input type="checkbox" v-model="settings.showAtoms" id="showAtoms" name="showAtoms"></label>
-    <label for="showCells">Show Cells<input type="checkbox" v-model="settings.showCells" id="showCells" name="showCells"></label>
-
-    <layerControl
-        v-for="(layer, index) in this.layers"
-        v-bind:key="index" v-bind:index="index" v-bind:data="layer"
-        @remove-layer="removeLayer"
-    ></layerControl>
-    <button @click="addLayer()">Add layer</button>
+        <label for="showAtoms">Show Atoms<input type="checkbox" v-model="settings.showAtoms" id="showAtoms" name="showAtoms"></label>
+        <label for="showCells">Show Cells<input type="checkbox" v-model="settings.showCells" id="showCells" name="showCells"></label>
+        
+        <input type="range" id="scale" name="scale" min="1" max="50" style="width: 30em;" v-model="settings.scale">
+        <label for="scale">Scale</label>
+        {{ settings.scale }}
+    
+        <layerControl
+            v-for="(layer, index) in this.layers"
+            v-bind:key="index" v-bind:index="index" v-bind:l="layer"
+            @remove-layer="removeLayer"
+        ></layerControl>
+        <button @click="addLayer()">Add layer</button>
+        
+        <div id="poscar">
+            <span style="color:red;" v-if="this.messages.poscarError">{{ messages.poscarError }} <br></span>
+            <label for="poscarArea">Paste POSCAR content here:<textarea name="poscarArea" v-model="inputs.poscar"></textarea></label>
+            <br>
+            <label for="poscarSpecies">Atomic elements (if not supplied in the POSCAR, as a SPACE-separated list of symbols):
+                <input type="text" name="poscarSpecies" v-model="inputs.poscarSpecies">
+            </label>
+            <br>
+            <button @click="loadPoscar()">Load layer from POSCAR</button>
+            <br>
+            Note: we ignore $$v_1^z, v_2^z, v_3^x,$$ and $$v_3^y$$ in the parsing, and we assume $$v_3^z$$ to be the "height" (layer thickness) of the unit cell.
+        </div>
     </nav>
 </main>
 `,
@@ -199,6 +285,16 @@ let lp = new Vue({
                 rotation: 0
             });
         },
+
+        loadPoscar: function () {
+            let res = parsePoscar(this.inputs.poscar, this.inputs.poscarSpecies.trim().split(/\s+/));
+            if (res.error !== undefined) {
+                this.messages.poscarError = res.error;
+            } else {
+                this.layers.push(res);
+            }
+        },
+
         removeLayer: function (index) {
             this.layers.splice(index, 1);
         }
